f64201e318864303b16e1395912fbb32488d293f
Jason Polites <jason.polites@gmail.com>
2023-09-14 21:39:44 -0700

    Significant code changes. Basically re-wrote most of it

--- extension/js/tabsweeper.js
+++ extension/js/tabsweeper.js
@@ -1,21 +1,17 @@
 // Globals
-var hWndOptionsOpen = false;
-var hWndOptionsId;
-var hWndOptionsTabId;
-var pendingCloseEvents = 0;
-var hwndCurrentId;
-var rules;
-var showWarning = false;
-var TYPE_URL = 0;
-var TYPE_TITLE = 1;
-var TYPE_DEFAULT = 2;
+let pendingCloseEvents = 0;
+let hwndCurrentId;
+let rules;
+const TYPE_URL = 0;
+const TYPE_TITLE = 1;
+const TYPE_DEFAULT = 2;
 
-function closeDuplicateTabs(callback) {
+const closeDuplicateTabs = (callback) => {
   getDuplicateTabs(dupes => {
-    var num = dupes?.length;
+    let num = dupes?.length;
     pendingCloseEvents = num;
     if (num > 0) {
-      for (var i = 0; i < dupes.length; i++) {
+      for (let i = 0; i < dupes.length; i++) {
         chrome.tabs.remove(dupes[i].id, () => {
           if (--num === 0) {
             setBadgeValue(0);
@@ -33,34 +29,34 @@ function closeDuplicateTabs(callback) {
   });
 }
 
-function getDuplicateTabs(callback) {
-  var self = this;
+const getDuplicateTabs = (callback) => {
+  let self = this;
 
   // Get the current active tab in the lastly focused window
   chrome.tabs.query({
     // active: true,
     // lastFocusedWindow: true
   }, tabs => {
-    var dupes = [];
+    let dupes = [];
 
     if (tabs) {
-      var all = [];
+      let all = [];
 
-      for (var i = 0; i < tabs.length; i++) {
+      for (let i = 0; i < tabs.length; i++) {
 
         // Get for exclusions
-        var type = TYPE_DEFAULT;
-        var bFullUrl = false;
-        for (var rule in rules) {
+        let type = TYPE_DEFAULT;
+        let bFullUrl = false;
+        for (let rule in rules) {
           if (tabs[i].url.match(rule) != null) {
-            type = parseInt(rules[rule].type);
-            bFullUrl = rules[rule].full === true;
+            type = parseInt(rules[rule].detectType);
+            bFullUrl = rules[rule].bUseFullUrl === true;
             break;
           }
         }
 
         if (type >= 0) {
-          var id = getTabIdentifier(tabs[i], type, bFullUrl);
+          let id = getTabIdentifier(tabs[i], type, bFullUrl);
           if (!all[id]) {
             all[id] = tabs[i];
           } else {
@@ -82,84 +78,31 @@ function getDuplicateTabs(callback) {
       callback.call(self, dupes);
     }
   });
-
-
-
-  // chrome.tabs.getSelected(null, tab => {
-  //   var currentTabId = tab.id;
-
-  //   chrome.tabs.query({
-
-  //   }, (tabs) => {
-
-  //       var dupes = [];
-
-  //       if (tabs) {
-  //         var all = [];
-
-  //         for (var i = 0; i < tabs.length; i++) {
-
-  //           // Get for exclusions
-  //           var type = TYPE_DEFAULT;
-  //           var bFullUrl = false;
-  //           for (var rule in rules) {
-  //             if (tabs[i].url.match(rule) != null) {
-  //               type = parseInt(rules[rule].type);
-  //               bFullUrl = rules[rule].full === true;
-  //               break;
-  //             }
-  //           }
-
-  //           if (type >= 0) {
-  //             var id = getTabIdentifier(tabs[i], type, bFullUrl);
-  //             if (!all[id]) {
-  //               all[id] = tabs[i];
-  //             } else {
-  //               // Don't push this one if it's the current tab
-  //               if (tabs[i].id === currentTabId) {
-  //                 dupes.push(all[id]);
-  //               } else {
-  //                 dupes.push(tabs[i]);
-  //               }
-  //             }
-  //           }
-  //         }
-
-  //         all = null;  // Delete
-  //       }
-
-  //       if (callback) {
-  //         callback.call(self, dupes);
-  //       }
-  //     });    
-  // });
 }
 
-function setBadgeValue(val) {
+const setBadgeValue = (val) => {
   if (val == undefined) {
     getDuplicateTabs((dupes) => {
-      __setBadgeText(dupes.length);
-      var title;
+      setBadgeText(dupes.length);
+      let title;
       if (dupes.length > 0) {
         title = "Click to close these tabs:\n";
-        for (var i = 0; i < dupes.length; i++) {
+        for (let i = 0; i < dupes.length; i++) {
           title += "- " + dupes[i].title + "\n";
         }
       } else {
         title = "Close Duplicate Tabs";
       }
-
       chrome.action.setTitle({
         "title": title
       });
-
     });
   } else {
-    __setBadgeText(val);
+    setBadgeText(val);
   }
 }
 
-function __setBadgeText(val) {
+const setBadgeText = (val) => {
   if (val > 0) {
     chrome.action.setBadgeText({ text: '' + val });
   } else {
@@ -168,15 +111,20 @@ function __setBadgeText(val) {
       "title": "Close Duplicate Tabs"
     });
   }
+
+  chrome.action.setBadgeBackgroundColor(
+    {color: '#32CD32'},  // LimeGreen
+    () => { /* ... */ },
+  );
 }
 
-function getTabIdentifier(tab, type, bFullUrl) {
+const getTabIdentifier = (tab, type, bFullUrl) => {
 
   if (type === TYPE_TITLE) {
     return tab.title;
   }
 
-  var url = tab.url;
+  let url = tab.url;
 
   if (bFullUrl !== true) {
     if (url && url.indexOf('#') >= 0) {
@@ -195,42 +143,12 @@ function getTabIdentifier(tab, type, bFullUrl) {
   }
 }
 
-function launchSettingsModal() {
-  if (hWndOptionsOpen === true) {
-    chrome.windows.update(hWndOptionsId, { focused: true })
-  } else {
-    var w = 700;
-    var h = 500;
-    var l = (screen.width / 2) - (w / 2);
-    var t = (screen.height / 2) - (h / 2);
-    chrome.windows.create({
-      url: chrome.runtime.getURL('settings.html'),
-      type: 'popup',
-      width: w,
-      height: h,
-      left: l,
-      top: t,
-      focused: true
-    }, function (child) {
-      hWndOptionsId = child.id;
-      hWndOptionsTabId = child.tabs[0].id;
-      hWndOptionsOpen = true;
-    });
-
-    chrome.windows.onRemoved.addListener((windowId) => {
-      if (windowId == hWndOptionsId) {
-        hWndOptionsOpen = false;
-      }
-    });
-  }
-}
-
-function loadSettings(callback) {
-  var self = this;
+const loadOptions = (callback) => {
+  let self = this;
   chrome.storage.sync.get('rules', (result) => {
     if (result) {
       rules = result.rules;
-      showWarning = result.showWarning;
+      // showWarning = result.showWarning;
     } else {
       rules = {};
     }
@@ -240,58 +158,17 @@ function loadSettings(callback) {
   });
 }
 
-// Add addional setup functions here
-var setupFunctions = {
-  "1.1.1": [setup1_1_1]
-};
-
-function setup() {
-  chrome.storage.sync.get('setup', function (result) {
-    var version = chrome.runtime.getManifest().version;
-    if (!result || result !== version) {
-      var funcs = setupFunctions[version];
-      if (funcs && funcs.length) {
-        for (var i = 0; i < funcs.length; i++) {
-          funcs[i](version);
-        }
-      }
-      chrome.storage.sync.set({ 'setup': version });
-    }
-  });
-}
-
-// Additional setup for 1.1.1
-function setup1_1_1(_version) {
-  chrome.storage.sync.get('rules', (data) => {
-    if (!data || Object.keys(data).length <= 0) {
-      // Defaults
-      var newRules = {
-        "https://plx.corp.google.com/script/": {
-          "type": "-1",
-          "full": false
-        },
-        "https://www.google.com": {
-          "type": "1",
-          "full": false
-        },
-      };
-      chrome.storage.sync.set({ 'rules': newRules });
-    }
-  });
-}
-
 function main() {
 
-  setup();
-
   chrome.tabs.onUpdated.addListener((tabId, changeInfo, _tab) => {
-    if (tabId !== hWndOptionsTabId && changeInfo.status === 'complete') {
+    // if (tabId !== hWndOptionsTabId && changeInfo.status === 'complete') {
+    if (changeInfo.status === 'complete') {
       setBadgeValue();
     }
   });
 
   chrome.tabs.onRemoved.addListener((tabId, _changeInfo, _tab) => {
-    if (tabId !== hWndOptionsTabId) {
+    // if (tabId !== hWndOptionsTabId) {
       if (pendingCloseEvents === 0) {
         setBadgeValue();
       } else {
@@ -300,37 +177,18 @@ function main() {
           pendingCloseEvents = 0;
         }
       }
-    }
+    // }
   });
 
   chrome.action.onClicked.addListener(() => {
     closeDuplicateTabs();
   });
 
-  // try {
-  //   chrome.contextMenus.create({
-  //     id: 'show-settings', // or any other name
-  //     title: 'Tab Sweeper Settings',
-  //     contexts: ['browser_action'],
-  //     visible:true
-  //   }, () => {
-  //     if (chrome.runtime.lastError) {
-  //       // This will occasionally fail due to a duplicate ID, but there is no "exists" method
-  //       // in the API to avoid this
-  //       console.warn(chrome.runtime.lastError.message);
-  //     }
-  //   });
-  // } catch (ignore) {} // Catch exception due to https://code.google.com/p/chromium/issues/detail?id=496020
-
-  // chrome.contextMenus.onClicked.addListener(() => {
-  //   launchSettingsModal();
-  // });
-
   //Add a listener to respond to changes in settings
   chrome.runtime.onMessage.addListener((message, _sender, sendResponse) => {
     if (message.action) {
       if (message.action === 'update_exclusions') {
-        loadSettings(() => {
+        loadOptions(() => {
           setBadgeValue();
           sendResponse();
         });
@@ -343,7 +201,7 @@ function main() {
   chrome.windows.getCurrent({ populate: false }, (wnd) => {
     hwndCurrentId = wnd.id;
     // Get Settings
-    loadSettings(() => {
+    loadOptions(() => {
       setBadgeValue();
     });
   });
